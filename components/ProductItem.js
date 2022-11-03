df8c397018f72a4ee0f16e0d3d9bf0b63ef6982f
Levophobia <christoffer.sahlin@hotmail.com>
2022-11-03 20:06:45 +0100

    dataadded

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,24 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Link from "next/link";
 
-export default function ProductItem(product) {
+export default function ProductItem({product}) {
   return (
     <div className="card">
-      <Link href={`/product/${product.slug}`}>
+      <Link legacyBehavior href={`/product/${product.slug}`}>
         <a>
-          shadow
           <img
             src={product.image}
             alt={product.name}
             className="rounded shadow"
-          ></img>
+          />
         </a>
       </Link>
 
       <div className="flex flex-col items-center justify-center p-5">
-        <Link href={`/product/${product.slug}`}>
+        <Link legacyBehavior href={`/product/${product.slug}`}>
         <a>
-        <h2 className="text-leg">
+        <h2 className="text-lg">
             {product.name}
         </h2>
         </a>
--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <Layout title="Home Page">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {data.products.map((product) => 
+        {data.products.map((product) => (
         <ProductItem product = {product} key={product.slug}/>
-        )}
+        ))}
       </div>
     </Layout>
   );
